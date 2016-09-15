96aec2eacdb4a216da21c7fd145f92a24cdcdf57
rcc <rcc>
2016-09-15 11:36:22 -0700

    Signed-off-by: rcc <rcc>

---
+++ homework/Gaddis_ch2_prob14/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on July 19, 2016, 9:07 AM
+ * Purpose:  Hello World Template
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    cout<<"Anthony Caccamise"<<endl;
+    cout<<"1468 Magnolia ave, Riverside, CA, 92504"<<endl;
+    cout<<"6875309"<<endl;
+    cout<<"Computer Science"<<endl;
+    //Exit
+    return 0;
+}
+
